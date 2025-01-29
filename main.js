5d81e33c1666263e52271cd8ff8d8209cc35df5d
Huy Nguyen <huyn.nguyen95@gmail.com>
2025-01-29 09:38:07 -0500

    hamburger menu toggle functionality

---
+++ main.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector(
+  '[aria-controls="primary-navigation"]',
+);
+const primaryNav = document.querySelector("#primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  const navOpened = navToggle.getAttribute("aria-expanded") === "true";
+
+  navToggle.setAttribute("aria-expanded", !navOpened);
+});
